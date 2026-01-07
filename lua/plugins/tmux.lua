6691c26962e2443493ed49a986f15b998ccd67fb
Kim Woo-yeong <wooyeong@monymony.co>
2026-01-07 18:33:49 +0900

    chore: update nvim navigation

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -2,11 +2,11 @@ return {
   {
     "christoomey/vim-tmux-navigator",
     keys = {
-      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", mode = "n", desc = "Tmux navigate left" },
-      { "<C-j>", "<cmd>TmuxNavigateDown<cr>", mode = "n", desc = "Tmux navigate down" },
-      { "<C-k>", "<cmd>TmuxNavigateUp<cr>", mode = "n", desc = "Tmux navigate up" },
-      { "<C-l>", "<cmd>TmuxNavigateRight<cr>", mode = "n", desc = "Tmux navigate right" },
-      { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", mode = "n", desc = "Tmux navigate previous" },
+      { "<M-h>", "<cmd>TmuxNavigateLeft<cr>", mode = "n", desc = "Tmux navigate left" },
+      { "<M-j>", "<cmd>TmuxNavigateDown<cr>", mode = "n", desc = "Tmux navigate down" },
+      { "<M-k>", "<cmd>TmuxNavigateUp<cr>", mode = "n", desc = "Tmux navigate up" },
+      { "<M-l>", "<cmd>TmuxNavigateRight<cr>", mode = "n", desc = "Tmux navigate right" },
+      { "<M-\\>", "<cmd>TmuxNavigatePrevious<cr>", mode = "n", desc = "Tmux navigate previous" },
     },
   },
 }
